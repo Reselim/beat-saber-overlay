930dee551d52f46b40b0a5129157177b8c56bd08
Reselim <reselimiscool@gmail.com>
2018-08-27 18:44:47 -0400

    a

--- js/modifiers.js
+++ js/modifiers.js
@@ -1,11 +1,11 @@
 if (location.hash) {
-	var modifiers = location.hash.splice(1).split(",");
+	var modifiers = location.hash.slice(1).split(",");
 
 	modifiers.forEach((modifier) => {
 		var link = document.createElement("link");
 		link.setAttribute("rel", "stylesheet");
 		link.setAttribute("href", "./modifiers/" + modifier + ".css");
-		
+
 		document.head.appendChild(link);
 	});
 }
